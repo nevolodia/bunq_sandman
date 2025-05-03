41e7424fcf7799ff59343671a016feb2aa9df1b0
Egor Georgievskii <e.georgievskii@student.tudelft.nl>
2025-05-03 13:05:32 +0200

    Event queue messages accept action index and message type

--- interpret.py
+++ interpret.py
@@ -12,56 +12,56 @@ class BunqInterpreter:
         self.iban_alias_for_account = {}
 
     def interpret(self, actions, event_queue):
-        for action in actions:
+        for action_i, action in enumerate(actions):
             action_type = action.get("action_type", event_queue)
             if action_type == "CreateUserPerson":
                 try:
                     self._create_user_person(action, event_queue)
-                    event_queue.put(f"User created successfully")
+                    event_queue.put({ "action_index": action_i, "type": "success", "message": f"User created successfully"})
                 except Exception as e:
-                    event_queue.put(f"Error creating user: {e}")
+                    event_queue.put({ "action_index": action_i, "type": "error", "message": f"Error creating user: {e}" })
             elif action_type == "LoginUserPerson":
                 try:
                     self._login_user_person(action, event_queue)
-                    event_queue.put(f"User logged in successfully")
+                    event_queue.put({ "action_index": action_i, "type": "success", "message": f"User logged in successfully" })
                 except Exception as e:
-                    event_queue.put(f"Error logging in user: {e}")
+                    event_queue.put({ "action_index": action_i, "type": "error", "message": f"Error logging in user: {e}" })
             elif action_type == "CreateMonetaryAccount":
                 try:
                     self._create_monetary_account(action, event_queue)
-                    event_queue.put(f"Monetary account created successfully")
+                    event_queue.put({ "action_index": action_i, "type": "success", "message": f"Monetary account created successfully" })
                 except Exception as e:
-                    event_queue.put(f"Error creating monetary account: {e}")
+                    event_queue.put({ "action_index": action_i, "type": "error", "message": f"Error creating monetary account: {e}" })
             elif action_type == "GetAccountOverview":
                 try:
-                    self._get_account_overview(action, event_queue)
-                    event_queue.put(f"Account overview retrieved successfully")
+                    self._get_account_overview(action, event_queue, action_i)
+                    event_queue.put({"action_index": action_i, "type": "success", "message": "Account overview retrieved successfully"})
                 except Exception as e:
-                    event_queue.put(f"Error retrieving account overview: {e}")
+                    event_queue.put({"action_index": action_i, "type": "error", "message": f"Error retrieving account overview: {e}"})
             elif action_type == "MakePayment":
                 try:
                     self._make_payment(action, event_queue)
-                    event_queue.put(f"Payment made successfully")
+                    event_queue.put({ "action_index": action_i, "type": "success", "message": "Payment made successfully" })
                 except Exception as e:
-                    event_queue.put(f"Error making payment: {e}")
+                    event_queue.put({ "action_index": action_i, "type": "error", "message": f"Error making payment: {e}" })
             elif action_type == "RequestPayment":
                 try:
                     self._request_payment(action, event_queue)
-                    event_queue.put(f"Payment request sent successfully")
+                    event_queue.put({ "action_index": action_i, "type": "success", "message": "Payment request sent successfully" })
                 except Exception as e:
-                    event_queue.put(f"Error sending payment request: {e}")
+                    event_queue.put({ "action_index": action_i, "type": "error", "message": f"Error sending payment request: {e}" })
             elif action_type == "RespondToPaymentRequest":
                 try:
-                    self._respond_to_payment_request(action, event_queue)
-                    event_queue.put(f"Responded to payment request successfully")
+                    self._respond_to_payment_request(action, event_queue, action_i)
+                    event_queue.put({ "action_index": action_i, "type": "success", "message": "Responded to payment request successfully" })
                 except Exception as e:
-                    event_queue.put(f"Error responding to payment request: {e}")
+                    event_queue.put({ "action_index": action_i, "type": "error", "message": f"Error responding to payment request: {e}" })
             elif action_type == "Sleep":
                 sleep_time = action.get("seconds", 1)
-                event_queue.put(f"Sleeping for {sleep_time} seconds")
+                event_queue.put({"type": "success", "message": f"Sleeping for {sleep_time} seconds"})
                 time.sleep(sleep_time)
             else:
-                event_queue.put(f"Unknown action type: {action_type}")
+                event_queue.put({"action_index": action_i, "type": "error", "message": f"Unknown action type: {action_type}"})
 
     def _create_user_person(self, action, event_queue):
         user_id = action.get("user_id")
@@ -84,13 +84,13 @@ class BunqInterpreter:
         account = api.get_account(user_id, account_id_bunq)
         self.iban_alias_for_account[account_id_bunq] = api.get_iban_alias(account)
 
-    def _get_account_overview(self, action, event_queue):
+    def _get_account_overview(self, action, event_queue, action_i):
         account_id = self.account_map[action["account_id"]]
         user_id = self.user_for_account[account_id]
         
         overview = api.get_account(user_id, account_id)
         balance = overview.MonetaryAccountBank.balance.value
-        event_queue.put(f"Account: {action['account_id']}, Balance: {balance}")
+        event_queue.put({ "action_index": action_i, "type": "log", "message": f"Account {action['account_id']} balance: {balance}"})
 
     def _make_payment(self, action, event_queue):
         account_id = self.account_map[action["account_id"]]
@@ -122,14 +122,14 @@ class BunqInterpreter:
         description = action.get("description", "No description")
         api.create_payment_request(user_id, account_id, amount_value, amount_currency, counterparty_alias, description)
 
-    def _respond_to_payment_request(self, action, event_queue):
+    def _respond_to_payment_request(self, action, event_queue, action_i):
         account_id = self.account_map[action["account_id"]]
         user_id = self.user_for_account[account_id]
         status = action.get("status", "REJECTED")
         counterparty_account_id = self.account_map[action["counterparty_account_id"]]
         counterparty_iban_alias = self.iban_alias_for_account[counterparty_account_id]
         updated_requests_id = api.respond_to_payment_request(user_id, account_id, counterparty_iban_alias, status)
-        event_queue.put(f"Responded to  {len(updated_requests_id)} requests for the user {action['account_id']}")
+        event_queue.put( {"action_index": action_i, "type": "log", "message": f"Responded to  {len(updated_requests_id)} requests for the user {action['account_id']}" })
 
 
 def test_create_user_and_accounts():
@@ -143,7 +143,7 @@ def test_create_user_and_accounts():
         {"action_type": "Sleep", "seconds": 5},
         {"action_type": "RequestPayment", "user_id": 1, "account_id": "B", "amount_value": 5.00, "amount_currency": "EUR", "counterparty_account_id": "A"},
         {"action_type": "Sleep", "seconds": 1},
-        {"action_type": "RespondToPaymentRequest", "user_id": 0, "account_id": "A", "counterparty_account_id": "B", "status": "REJECTED"},
+        {"action_type": "RespondToPaymentRequest", "user_id": 0, "account_id": "A", "counterparty_account_id": "B", "status": "ACCEPTED"},
         {"action_type": "Sleep", "seconds": 1},
         {"action_type": "GetAccountOverview", "user_id": 1, "account_id": "B"},
     ]
